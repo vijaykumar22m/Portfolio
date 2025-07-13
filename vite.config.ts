abdc987d7b806ab6f5218624524889f39280d05a
vijay kumar <mvijaykumardi@gmail.com>
2025-07-13 09:38:12 +0530

    deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
+// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Portfolio/', // 👈 your repo name with slashes
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
 });
+
