c24c6318ab210e0dfc517e01c108c18ebd5dfbc0
vijaykumar22m <mvijaykumardi@gmail.com>
2025-07-13 11:33:16 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Portfolio/', // 👈 your repo name with slashes
+  // base: '/Portfolio/', // 👈 your repo name with slashes
+    base: './', // or '/' also works
+
   plugins: [react()],
 });
 
